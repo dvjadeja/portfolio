6e17e11ce298b54ec6391cee7c23c938ee98daa9
Dilipsinh Jadeja <dilipsinh.j@unomok.com>
2021-10-18 10:20:44 +0530

    First Commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+import one from "./img/1.jpg";
+import two from "./img/2.jpg";
+import three from "./img/3.jpg";
+import four from "./img/4.jpg";
+import five from "./img/5.jpg";
+import six from "./img/6.jpg";
+
+export const products = [
+  {
+    id: 1,
+    img: one,
+    link: "http://lama.dev",
+  },
+  {
+    id: 2,
+    img: two,
+    link: "http://lama.dev",
+  },
+  {
+    id: 3,
+    img: three,
+    link: "http://lama.dev",
+  },
+  {
+    id: 4,
+    img: four,
+    link: "http://lama.dev",
+  },
+  {
+    id: 5,
+    img: five,
+    link: "http://lama.dev",
+  },
+  {
+    id: 6,
+    img: six,
+    link: "http://lama.dev",
+  },
+];
